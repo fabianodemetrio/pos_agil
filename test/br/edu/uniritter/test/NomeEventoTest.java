cf1667f2dddf460075aeba1be43465fafac00101
bagep <bage.persch@gmail.com>
2016-09-24 09:52:55 -0300

    Refatorada a classe NomeEventoTest

--- test/br/edu/uniritter/test/NomeEventoTest.java
+++ test/br/edu/uniritter/test/NomeEventoTest.java
@@ -3,33 +3,38 @@ package br.edu.uniritter.test;
 import java.util.Arrays;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import br.edu.uniritter.service.EventoService;
+import br.edu.uniritter.validator.ValidadorEvento;
 
 public class NomeEventoTest {
 
+	private ValidadorEvento validador;
+	
 	private String caracteres(int tamanho) {
 		char[] caracteres = new char[tamanho];
 		Arrays.fill(caracteres, 'T');
 		return new String(caracteres);
 	}
+	
+	@Before
+	public void setUp(){
+		this.validador = new ValidadorEvento();
+	}
 
 	@Test
 	public void nomeDoEventoMaiorQue150Caracteres() {
-		Assert.assertEquals(true, new EventoService()
-				.nomeEventoMaiorIgual150Caracteres(caracteres(151)));
+		Assert.assertEquals(true, this.validador.nomeEventoMaiorIgual150Caracteres(caracteres(151)));
 	}
 
 	@Test
 	public void nomeDoEventoMenorQue150Caracteres() {
-		Assert.assertEquals(false, new EventoService()
-				.nomeEventoMaiorIgual150Caracteres(caracteres(50)));
+		Assert.assertEquals(false, this.validador.nomeEventoMaiorIgual150Caracteres(caracteres(50)));
 	}
 
 	@Test
 	public void nomeDoEventoIgual150Caracteres() {
-		Assert.assertEquals(false, new EventoService()
-				.nomeEventoMaiorIgual150Caracteres(caracteres(150)));
+		Assert.assertEquals(false, this.validador.nomeEventoMaiorIgual150Caracteres(caracteres(150)));
 	}
 }
