43c578ef40b858a0f38ce6f12003a9423dda31ef
fabianodemetrio <fabianodemetrio@gmail.com>
2016-09-24 09:27:32 -0300

    Refatoração classe salvar evento

--- src/test/java/br/edu/uniritter/test/SalvarEventoTest.java
+++ src/test/java/br/edu/uniritter/test/SalvarEventoTest.java
@@ -1,6 +1,7 @@
 package br.edu.uniritter.test;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,35 +10,41 @@ import br.edu.uniritter.model.Evento;
 import br.edu.uniritter.service.EventoService;
 
 public class SalvarEventoTest {
+	
+	private String caracteres(int tamanho) {
+		char[] caracteres = new char[tamanho];
+		Arrays.fill(caracteres, 'F');
+		return new String(caracteres);
+	}
 
 	@Test
 	public void salvarEventoMaiorQue150Caracteres() {
-		Assert.assertEquals(false, new EventoService().salvar(new Evento("Evento MUITO GRANDEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEee", LocalDate.now())));
+		Assert.assertEquals(false, new EventoService().salvar(new Evento(caracteres(200), LocalDate.now())));
 	}
 	
 	@Test
 	public void salvarEventoMenorQue150Caracteres() {
-		Assert.assertEquals(true, new EventoService().salvar(new Evento("Evento X", LocalDate.now())));
+		Assert.assertEquals(true, new EventoService().salvar(new Evento(caracteres(149), LocalDate.now())));
 	}
 	
 	@Test
 	public void salvarEventoIgualQue150Caracteres() {
-		Assert.assertEquals(true, new EventoService().salvar(new Evento("EventoCom150Caracteres:grandeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", LocalDate.now())));
+		Assert.assertEquals(true, new EventoService().salvar(new Evento(caracteres(150), LocalDate.now())));
 	}
 	
 	 @Test
 	public void salvarEventoDataMenorQueAtual() {
-			Assert.assertEquals(false, new EventoService().salvar(new Evento("Evento", LocalDate.now().minusDays(1L))));
+			Assert.assertEquals(false, new EventoService().salvar(new Evento(caracteres(149), LocalDate.now().minusDays(1L))));
 	}
 	
 	 @Test
 	public void salvarEventoDataMaiorQueAtual() {
 
-		Assert.assertEquals(true, new EventoService().salvar(new Evento("Evento X", LocalDate.now().plusDays(1L))));
+		Assert.assertEquals(true, new EventoService().salvar(new Evento(caracteres(149), LocalDate.now().plusDays(1L))));
 	}
 	
 	@Test
 	public void salvarEventoDataIgualQueAtual() {
-		Assert.assertEquals(true, new EventoService().salvar(new Evento("Evento X", LocalDate.now())));
+		Assert.assertEquals(true, new EventoService().salvar(new Evento(caracteres(149), LocalDate.now())));
 	}
 }
