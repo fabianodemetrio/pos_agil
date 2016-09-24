ac96660fcf6b6c9090474b9d00ea7c1b82dc92f2
yurikilian <ykilian2006@gmail.com>
2016-09-24 09:22:30 -0300

    refatoração teste

--- src/test/java/br/edu/uniritter/test/NomeEventoTest.java
+++ src/test/java/br/edu/uniritter/test/NomeEventoTest.java
@@ -1,23 +1,35 @@
 package br.edu.uniritter.test;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import br.edu.uniritter.service.EventoService;
 
 public class NomeEventoTest {
+
+	private String caracteres(int tamanho) {
+		char[] caracteres = new char[tamanho];
+		Arrays.fill(caracteres, 'T');
+		return new String(caracteres);
+	}
+
 	@Test
 	public void nomeDoEventoMaiorQue150Caracteres() {
-		Assert.assertEquals(true, new EventoService().nomeEventoMaiorIgual150Caracteres("adsdasdsadsadsasdsadadsadasdasdsadasdsadasdasdasdsadaadsdasdsadsadsasdsadadsadasdasdsadasdsadasdasdasdsadaadsdasdsadsadsasdsadadsadasdasdsadasdsadasdasdasdsadaadsdasdsadsadsasdsadadsadasdasdsadasdsadasdasdasdsada"));
+		Assert.assertEquals(true, new EventoService()
+				.nomeEventoMaiorIgual150Caracteres(caracteres(151)));
 	}
 
 	@Test
 	public void nomeDoEventoMenorQue150Caracteres() {
-		Assert.assertEquals(false, new EventoService().nomeEventoMaiorIgual150Caracteres("teste"));
+		Assert.assertEquals(false, new EventoService()
+				.nomeEventoMaiorIgual150Caracteres(caracteres(50)));
 	}
 
 	@Test
 	public void nomeDoEventoIgual150Caracteres() {
-		Assert.assertEquals(false, new EventoService().nomeEventoMaiorIgual150Caracteres("adsdasdsadsadsasdsadadsadasdasdsadasdsadasdasdasdsadaadsdasdsadsadsasdsadadsadasdasdsadasdsadasdasdasdsadaadsdasdsadsadsasdsadadsadasdasdsadasdsadasda"));
+		Assert.assertEquals(false, new EventoService()
+				.nomeEventoMaiorIgual150Caracteres(caracteres(150)));
 	}
 }
