4c2df59f02208d533961743e88fd2de9a2105c6f
yurikilian <ykilian2006@gmail.com>
2016-10-01 10:12:38 -0300

    Mais testes Signed-off-by: yurikilian <ykilian2006@gmail.com>

--- test/br/edu/uniritter/calculadora/CalculadoraDescontoTest.java
+++ test/br/edu/uniritter/calculadora/CalculadoraDescontoTest.java
@@ -8,21 +8,44 @@ import java.math.RoundingMode;
 import org.junit.Test;
 
 import br.edu.uniritter.model.calculadora.CalculadoraDesconto;
+import br.edu.uniritter.model.desconto.DescontoEstudante;
 import br.edu.uniritter.model.desconto.DescontoGold;
+import br.edu.uniritter.model.desconto.DescontoIdoso;
 import br.edu.uniritter.model.desconto.DescontoSilver;
 import br.edu.uniritter.model.ingresso.IngressoVip;
 
 public class CalculadoraDescontoTest {
 	CalculadoraDesconto calculaDesconto = new CalculadoraDesconto();
-	
+
 	@Test
-	public void deveAplicarDescontoGold(){
-		assertEquals(new BigDecimal(250.00).setScale(2,RoundingMode.HALF_DOWN), calculaDesconto.calcular(new IngressoVip(), new DescontoGold()));
+	public void deveAplicarDescontoGold() {
+		assertEquals(
+				new BigDecimal(250.00).setScale(2, RoundingMode.HALF_DOWN),
+				calculaDesconto.calcular(new IngressoVip(), new DescontoGold()));
 	}
-	
+
 	@Test
-	public void deveAplicarDescontoSilver(){
-		assertEquals(new BigDecimal(400.00).setScale(2,RoundingMode.HALF_DOWN), calculaDesconto.calcular(new IngressoVip(), new DescontoSilver()));
+	public void deveAplicarDescontoSilver() {
+		assertEquals(
+				new BigDecimal(400.00).setScale(2, RoundingMode.HALF_DOWN),
+				calculaDesconto.calcular(new IngressoVip(),
+						new DescontoSilver()));
 	}
-	
+
+	@Test
+	public void deveAplicarDescontoIdoso() {
+		assertEquals(
+				new BigDecimal(500.00).setScale(2, RoundingMode.HALF_DOWN),
+				calculaDesconto
+						.calcular(new IngressoVip(), new DescontoIdoso()));
+	}
+
+	@Test
+	public void deveAplicarDescontoEstudante() {
+		assertEquals(
+				new BigDecimal(500.00).setScale(2, RoundingMode.HALF_DOWN),
+				calculaDesconto.calcular(new IngressoVip(),
+						new DescontoEstudante()));
+	}
+
 }
